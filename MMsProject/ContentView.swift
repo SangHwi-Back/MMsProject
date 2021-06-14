f96f9edf5bb4b2e1e08e4f70a30201b64c29d17a
백상휘 <sanghwiback@gmail.com>
2021-06-14 23:14:34 +0900

    Initial Commit

---
+++ MMsProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MMsProject
+//
+//  Created by 백상휘 on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MMsProject/MMsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMsProjectApp.swift
+//  MMsProject
+//
+//  Created by 백상휘 on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct MMsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
